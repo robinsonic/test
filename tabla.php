011fcb4a277c60e719d0db9bbdb8724bca8dfd61
robinsonic <binosonic@gmail.com>
2014-09-17 11:49:56 +0200

    Create tabla.php

---
+++ tabla.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+
+    <?php echo $_GET["name"]; 
+    $num=$_GET["name"]
+    ?> 
+
+    <table border="1">
+    	<?php
+    	for ($i=0; $i < $num; $i++) { 
+    		# code...
+    	
+    echo "<tr>
+<td>111</td>
+<td>111</td>
+        </tr>";
+        }
+        ?>
+    </table>
+
+
+</body>
+</html>
